17262a0921dd5355e240a32455fbea78ed259cf7
Tristan POISSON <poisson.tristan@gmail.com>
2021-11-19 19:30:12 +0000

    Milestone 1

---
+++ app/src/main/java/com/example/minesweeper/Minesweeper.kt
@@ -0,0 +1,10 @@
+package com.example.minesweeper
+
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.View
+
+class Minesweeper (context: Context, attrs: AttributeSet) : View(context, attrs){
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+}
