e167a9caaec87d8d0237defeda9783c6ca066988
Tristan POISSON <poisson.tristan@gmail.com>
2021-11-25 17:30:12 +0000

    Milestone 2

--- app/src/main/java/com/example/minesweeper/Minesweeper.kt
+++ app/src/main/java/com/example/minesweeper/Minesweeper.kt
@@ -7,4 +7,43 @@ import android.view.View
 
 class Minesweeper (context: Context, attrs: AttributeSet) : View(context, attrs){
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    private var cellColor = Color.BLACK
+    private var backColor = Color.WHITE
+
+    private var sizeH = 10
+    private var sizeW = 10
+
+    private var cellSize = 100
+    
+    override fun onDraw(canvas: Canvas){
+        var x = 0
+        var y = 0
+
+        drawBackground(canvas)
+        while (x < sizeW) {
+            while(y < sizeH){
+                drawCell(canvas, x, y, cellColor)
+                y++
+            }
+            y = 0
+            x++
+        }
+    }
+
+    private fun drawBackground(canvas: Canvas){
+        paint.color = backColor
+        paint.style = Paint.Style.FILL
+
+        val backgroud = Rect(25, 0, ((cellSize + 2) * sizeH)+ 10, ((cellSize + 2) * sizeW))
+        canvas.drawRect(backgroud, paint)
+    }
+
+    private fun drawCell(canvas: Canvas, posX: Int, posY: Int, color: Int) {
+        paint.color = color
+        paint.style = Paint.Style.FILL
+
+        val cell = Rect((posX * cellSize) + 30, (posY * cellSize) + 5, ((posX  * cellSize) + cellSize) + 25, (posY  * cellSize) + cellSize)
+        canvas.drawRect(cell, paint)
+    }
 }
