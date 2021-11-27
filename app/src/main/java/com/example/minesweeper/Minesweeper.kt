9ebaa6d48b0ab69f08d1ce6ff2e742491e8f48ef
Tristan POISSON <poisson.tristan@gmail.com>
2021-11-27 14:40:12 +0000

    Milestone 3

--- app/src/main/java/com/example/minesweeper/Minesweeper.kt
+++ app/src/main/java/com/example/minesweeper/Minesweeper.kt
@@ -1,8 +1,11 @@
 package com.example.minesweeper
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.util.Log
+import android.view.MotionEvent
 import android.view.View
 
 class Minesweeper (context: Context, attrs: AttributeSet) : View(context, attrs){
@@ -10,12 +13,14 @@ class Minesweeper (context: Context, attrs: AttributeSet) : View(context, attrs)
 
     private var cellColor = Color.BLACK
     private var backColor = Color.WHITE
+    private var digColor = Color.GRAY
 
     private var sizeH = 10
     private var sizeW = 10
 
     private var cellSize = 100
-    
+    private var gameBoard = Array(sizeH){ IntArray(sizeW){0} }
+
     override fun onDraw(canvas: Canvas){
         var x = 0
         var y = 0
@@ -23,12 +28,27 @@ class Minesweeper (context: Context, attrs: AttributeSet) : View(context, attrs)
         drawBackground(canvas)
         while (x < sizeW) {
             while(y < sizeH){
-                drawCell(canvas, x, y, cellColor)
+                drawCell(canvas, x, y)
                 y++
             }
             y = 0
             x++
         }
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        super.onTouchEvent(event)
+        var touchX : Int = (event.x/100).toInt()
+        var touchY : Int= (event.y/100).toInt()
+
+        Log.i("X:", touchX.toString())
+        Log.i("Y:", touchY.toString())
+        if (touchX < sizeW && touchY < sizeW && touchX >= 0 && touchY >= 0)
+            gameBoard[touchX][touchY] = -2
+
+        invalidate()
+        return true
     }
 
     private fun drawBackground(canvas: Canvas){
@@ -39,8 +59,11 @@ class Minesweeper (context: Context, attrs: AttributeSet) : View(context, attrs)
         canvas.drawRect(backgroud, paint)
     }
 
-    private fun drawCell(canvas: Canvas, posX: Int, posY: Int, color: Int) {
-        paint.color = color
+    private fun drawCell(canvas: Canvas, posX: Int, posY: Int) {
+        if (gameBoard[posX][posY] == 0)
+            paint.color = cellColor
+        else if (gameBoard[posX][posY] == -2)
+            paint.color = digColor
         paint.style = Paint.Style.FILL
 
         val cell = Rect((posX * cellSize) + 30, (posY * cellSize) + 5, ((posX  * cellSize) + cellSize) + 25, (posY  * cellSize) + cellSize)
